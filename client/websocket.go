2126f55c873df13151317af928ca862cb3c69723
Dan Toye <time4dan@gmail.com>
2015-11-05 13:42:26 -0800

    Anti-IP

--- client/websocket.go
+++ client/websocket.go
@@ -3,23 +3,50 @@ package client
 import (
 	"golang.org/x/net/websocket"
 	"log"
+	"sync"
 )
 
 type Server struct {
 	Room *Room
+	Lock sync.RWMutex
+	IPS  map[string]struct{}
 }
 
 func NewServer() *Server {
-	return &Server{}
+	return &Server{IPS: map[string]struct{}{}}
+}
+
+var allowedHosts = map[string]struct{}{
+	"http://www.tinybio.me": struct{}{},
+	"http://localhost:8080": struct{}{},
 }
 
 func (s *Server) Accept(ws *websocket.Conn) {
 	room := s.Room
-
-	log.Println("New Client", ws.RemoteAddr())
-	if ws.RemoteAddr().String() == "http://www.tinybio.me" {
-		room.Accept(NewJsonProtocol(ws))
+	a := ws.Request().RemoteAddr
+	reject := false
+	s.Lock.Lock()
+	if _, found := s.IPS[a]; found {
+		reject = true
+	} else {
+		s.IPS[a] = a
 	}
+	s.Lock.Unlock()
+
+	if _, found := allowedHosts[ws.RemoteAddr().String()]; !found {
+		reject = true
+	}
+
+	if !reject {
+		log.Println("New Client", a)
+		room.Accept(NewJsonProtocol(ws))
+	} else {
+		log.Println("REJECTING", a)
+	}
+
+	s.Lock.Lock()
+	delete(s.IPS, a)
+	s.Lock.Unlock()
 }
 
 func (s *Server) AddRoom(r *Room) {
